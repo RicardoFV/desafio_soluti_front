3836eb573acb53aa6d015bcdb7af42bb380e4c80
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2020-02-15 01:31:03 -0300

    criando a tela home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,58 @@
 <template>
-  <div>
-    <h1>Tabela de minhas empresas </h1>
+  <div class="container">
+      <table class="table table-sm table-bordered table-hover" width="80%">
+        <thead>
+            <tr align="center">
+                <h4 colspan=”6″>Minhas Empresas</h4>
+            </tr>
+            <tr align="center" >
+              <th scope="col">Razão Social</th>
+              <th scope="col">Nome Fantasia</th>
+              <th scope="col">CNPJ</th>
+              <th scope="col">Telefone</th>
+              <th scope="col">E-mail</th>
+              <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody v-for="empresa of empresas">
+            <tr>
+              <td>{{empresa.razao_social}}</td>
+              <td>{{empresa.nome_fantasia}}</td>
+              <td>{{empresa.cnpj}}</td>
+              <td>{{empresa.telefone}}</td>
+              <td>{{empresa.email}}</td>
+              <td>
+                <button>Ver</button>
+              </td>
+            </tr>
+        </tbody>
+      </table>
+
+
   </div>
 
 </template>
 <script>
+  import ServiceEmpresa from "../service/ServiceEmpresa";
+  import Button from "../components/button/Button";
+
   export default {
+    components: {Button},
+    data(){
+      return{
+        empresas:[]
+      }
+    },
+
+    created(){
+          this.serviceEmpresa = new ServiceEmpresa(this.$resource)
+          this.serviceEmpresa.listar()
+          .then(dados => this.empresas = dados, err =>{
+
+          })
+    }
   }
+
 </script>
 <style>
 
